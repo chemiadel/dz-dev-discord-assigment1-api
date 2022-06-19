8e1412f04f08ae9e80eedfb9ce1e9a8a9e7b5315
chemiadel <pace3man@gmail.com>
2022-06-19 10:04:20 +0100

    Edit: Allow cors

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ import LoginRoute from "./routes/login";
 import DataRoute from "./routes/data";
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.post("/login", LoginRoute);
 app.use("/data", DataRoute);
